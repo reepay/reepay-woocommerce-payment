9c2efb08eaed457db258121585e6830973e97d18
Robert Mygind <Robert@Innovated.dk>
2019-05-21 16:56:54 +0200

    Fixed problem with incorrect order handlers of renewal orders

--- reepay-woocommerce-payment.php
+++ reepay-woocommerce-payment.php
@@ -5,7 +5,7 @@
  * Description: Provides a Payment Gateway through Reepay for WooCommerce.
  * Author: AAIT
  * Author URI: #
- * Version: 1.1.10
+ * Version: 1.1.11
  * Text Domain: woocommerce-gateway-reepay-checkout
  * Domain Path: /languages
  * WC requires at least: 3.0.0
