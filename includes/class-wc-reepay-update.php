5fde1da78a69a40659e3aff493f63e5b56c9f80c
aait <info@aait.se>
2019-05-28 12:37:31 +0600

    Update db version

--- includes/class-wc-reepay-update.php
+++ includes/class-wc-reepay-update.php
@@ -9,6 +9,7 @@ class WC_Reepay_Update {
 	private static $db_updates = array(
 		'1.1.0' => 'updates/update-1.1.0.php',
 		'1.2.0' => 'updates/update-1.2.0.php',
+		'1.2.1' => 'updates/update-1.2.0.php',
 	);
 
 	/**
