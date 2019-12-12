8b54ea1cdc8b4e7761ac8b7a6c03449db2fd3fc7
aait <info@aait.se>
2019-12-12 13:26:36 +0600

    Implemented Overlay mode for "Pay order" page

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,4 +1,8 @@
 /* global wc_checkout_params */
+
+// Preload ModalCheckout
+window.rp = new Reepay.ModalCheckout();
+
 jQuery( function( $ ) {
     'use strict';
 
@@ -7,67 +11,95 @@ jQuery( function( $ ) {
         return false;
     }
 
-    // Preload ModalCheckout
-    var rp = new Reepay.ModalCheckout();
-
-    $(document).ajaxComplete( function ( event, xhr, settings ) {
+    $( document ).ajaxComplete( function ( event, xhr, settings ) {
         if ( settings.url === wc_checkout_params.checkout_url ) {
-            var data = xhr.responseText;
+            const data = xhr.responseText;
 
             // Parse
             try {
-                var result = $.parseJSON( data );
+                const result = $.parseJSON( data );
+
+                // Check is response from payment gateway
+                if ( ! result.hasOwnProperty( 'is_reepay_checkout' ) ) {
+                    return false;
+                }
+
+                wc_reepay.buildModalCheckout( result.reepay.id, result.accept_url, result.cancel_url );
             } catch ( e ) {
                 return false;
             }
-
-            // Check is response from payment gateway
-            if ( ! result.hasOwnProperty( 'is_reepay_checkout' ) ) {
-                return false;
-            }
-
-            var accept_url = result.accept_url,
-                cancel_url = result.cancel_url;
-
-
-            console.log(result);
-
-            if (WC_Gateway_Reepay_Checkout.payment_type === 'OVERLAY') {
-                // Show modal
-                rp.show(result.reepay.id);
-                //rp = new Reepay.ModalCheckout(result.reepay.id);
-            } else {
-                rp = new Reepay.WindowCheckout(result.reepay.id);
-            }
-
-            rp.addEventHandler(Reepay.Event.Accept, function(data) {
-                console.log('Accept', data);
-
-                var redirect_url = accept_url;
-                for (let prop in data) {
-                    redirect_url = setUrlParameter(redirect_url, prop, data[prop]);
-                }
-
-                window.location.href = redirect_url;
-            });
-
-            rp.addEventHandler(Reepay.Event.Cancel, function(data) {
-                console.log('Cancel', data);
-                window.location.href = cancel_url;
-            });
-
-            rp.addEventHandler(Reepay.Event.Close, function(data) {
-                console.log('Close', data);
-            });
-
-            rp.addEventHandler(Reepay.Event.Error, function(data) {
-                console.log('Error', data);
-                window.location.href = cancel_url;
-            });
         }
     } );
 
-    function setUrlParameter(url, key, value) {
+    $( document ).ready(function () {
+        if ( window.location.hash.indexOf( '#!reepay-pay' ) > -1 ) {
+            const url = document.location.hash.replace( '#!reepay-pay','' ),
+                params = new URLSearchParams( url );
+
+            let rid = params.get( 'rid' ),
+                accept_url = params.get( 'accept_url' ),
+                cancel_url = params.get( 'cancel_url' );
+
+            window.setTimeout( function () {
+                wc_reepay.buildModalCheckout( rid, accept_url, cancel_url );
+                history.pushState( '', document.title, window.location.pathname );
+            }, 1000 );
+        }
+    });
+});
+
+wc_reepay = {
+    /**
+     * Build Modal Checkout
+     *
+     * @param reepay_id
+     * @param accept_url
+     * @param cancel_url
+     */
+    buildModalCheckout: function ( reepay_id, accept_url, cancel_url ) {
+        if ( WC_Gateway_Reepay_Checkout.payment_type === 'OVERLAY' ) {
+            // Show modal
+            window.rp.show( reepay_id );
+            //rp = new Reepay.ModalCheckout( reepay_id );
+        } else {
+            window.rp = new Reepay.WindowCheckout( reepay_id );
+        }
+
+        window.rp.addEventHandler( Reepay.Event.Accept, function( data ) {
+            console.log( 'Accept', data );
+
+            let redirect_url = accept_url;
+            for ( let prop in data ) {
+                redirect_url = wc_reepay.setUrlParameter( redirect_url, prop, data[prop] );
+            }
+
+            window.location.href = redirect_url;
+        } );
+
+        window.rp.addEventHandler( Reepay.Event.Cancel, function( data ) {
+            console.log( 'Cancel', data );
+            window.location.href = cancel_url;
+        } );
+
+        window.rp.addEventHandler( Reepay.Event.Close, function( data ) {
+            console.log( 'Close', data );
+        } );
+
+        window.rp.addEventHandler( Reepay.Event.Error, function( data ) {
+            console.log( 'Error', data );
+            window.location.href = cancel_url;
+        } );
+    },
+
+    /**
+     * Add parameter for Url
+     *
+     * @param url
+     * @param key
+     * @param value
+     * @return {string}
+     */
+    setUrlParameter: function ( url, key, value ) {
         var baseUrl = url.split('?')[0],
             urlQueryString = '?' + url.split('?')[1],
             newParam = key + '=' + value,
@@ -95,5 +127,5 @@ jQuery( function( $ ) {
 
         return baseUrl + params;
     }
-});
+};
 
