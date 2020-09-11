9536cc2b89c4e5b3ef2e1808472717ab353e19aa
Robert Mygind <Robert@Innovated.dk>
2020-09-11 09:36:02 +0200

    Updated version to  1.2.7

--- reepay-woocommerce-payment.php
+++ reepay-woocommerce-payment.php
@@ -4,7 +4,7 @@
  * Description: Provides a Payment Gateway through Reepay for WooCommerce.
  * Author: reepay
  * Author URI: http://reepay.com
- * Version: 1.2.6
+ * Version: 1.2.7
  * Text Domain: woocommerce-gateway-reepay-checkout
  * Domain Path: /languages
  * WC requires at least: 3.0.0
