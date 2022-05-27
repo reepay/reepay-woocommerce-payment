a38e4dc17f3755a50264dfd26186e974fed40dc6
dimaspolohov@yandex.ru <Zenitkms123>
2022-05-27 16:40:24 +0300

    Fix function call

--- includes/class-wc-gateway-reepay-capture.php
+++ includes/class-wc-gateway-reepay-capture.php
@@ -61,9 +61,10 @@ class WC_Reepay_Order_Capture {
             return;
         }
 
-        $gateway = rp_get_payment_method( $order );
+        $gateways = WC()->payment_gateways()->get_available_payment_gateways();
+        $gateway = 	$gateways[ $payment_method ];
+        $invoice_data = $gateway->get_invoice_data($order);
 
-        $invoice_data = $gateway->api->get_invoice_data($order);
         if ( is_wp_error( $invoice_data ) ) {
             echo __( 'Invoice not found', 'reepay-checkout-gateway' );
             return;
