b4c7f6d9f2c435a78c15ffaf8ccc5fb84a4710da
dimaspolohov@yandex.ru <Zenitkms123>
2023-02-17 13:56:28 +0300

    Save credit card type and masked credit card number in order metadata#202

--- includes/class-wc-gateway-reepay-checkout.php
+++ includes/class-wc-gateway-reepay-checkout.php
@@ -669,7 +669,7 @@ class WC_Gateway_Reepay_Checkout extends WC_Gateway_Reepay {
 		$this->private_key_test = isset( $this->settings['private_key_test'] ) ? $this->settings['private_key_test'] : $this->private_key_test;
 		$this->test_mode        = isset( $this->settings['test_mode'] ) ? $this->settings['test_mode'] : $this->test_mode;
 
-        parent::is_webhook_configured();
+		parent::is_webhook_configured();
 
 		return true;
 	}
@@ -1197,6 +1197,9 @@ class WC_Gateway_Reepay_Checkout extends WC_Gateway_Reepay {
 				$token->set_card_type( $source['card_type'] );
 				$token->set_user_id( get_current_user_id() );
 				$token->set_masked_card( $source['masked_card'] );
+
+				update_post_meta( $id, 'reepay_masked_card', $source['masked_card'] );
+				update_post_meta( $id, 'reepay_card_type', $source['card_type'] );
 			}
 
 			// Save Credit Card
