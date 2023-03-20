15e8110687ea1316a26f495fffe7affd3d69fd58
daniel21703 <daniel217032001@gmail.com>
2023-03-20 21:38:42 +0300

    remove WC_Gateway_Reepay::get_calculated_amount

--- includes/abstracts/abstract-wc-gateway-reepay.php
+++ includes/abstracts/abstract-wc-gateway-reepay.php
@@ -1572,25 +1572,6 @@ abstract class WC_Gateway_Reepay extends WC_Payment_Gateway implements WC_Paymen
 	}
 
 	/**
-	 * calculate amount from order rows that
-	 * can be compared with amount from order
-	 *
-	 * @param $order
-	 *
-	 * @return float|int
-	 */
-	private function get_calculated_amount( $order ) {
-		$order_items = $this->get_order_items( $order );
-		$order_total = 0;
-
-		foreach ( $order_items as $item ) {
-			$order_total += $item['amount'] * $item['quantity'];
-		}
-
-		return $order_total;
-	}
-
-	/**
 	 * Get Language
 	 *
 	 * @return string
