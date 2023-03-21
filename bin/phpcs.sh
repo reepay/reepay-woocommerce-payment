f6a07b8abe913eb5cd7c80782accf3e940c73817
daniel21703 <daniel217032001@gmail.com>
2023-03-21 13:40:01 +0300

    update phpcs script

--- bin/phpcs.sh
+++ bin/phpcs.sh
@@ -4,6 +4,8 @@ standard='--standard=./ruleset.xml'
 path='./includes/*.php ./includes/**/*.php ./templates/**/*.php ./*.php'
 extra='--cache --colors -p -s' #remove colors if your terminal doesn't support them
 
+#exta+=' --report=diff -vvv' #uncomment for debug
+
 if [ "$1" == "-full" ]; then
   php ./vendor/bin/phpcs $standard $path $extra
 elif [ "$1" == '-fix' ]; then
