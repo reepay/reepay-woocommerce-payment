f01977cc1c1ce3f7897ab7d2404f29d87bc2b4ba
daniel21703 <daniel217032001@gmail.com>
2023-03-29 10:11:25 +0300

    fix Functions\Main

--- includes/Functions/Main.php
+++ includes/Functions/Main.php
@@ -6,11 +6,11 @@ defined( 'ABSPATH' ) || exit();
 
 class Main {
 	public function __construct() {
-		include_once dirname( __FILE__ ) . './currency.php';
-		include_once dirname( __FILE__ ) . './customer.php';
-		include_once dirname( __FILE__ ) . './format.php';
-		include_once dirname( __FILE__ ) . './gateways.php';
-		include_once dirname( __FILE__ ) . './order.php';
-		include_once dirname( __FILE__ ) . './subscriptions.php';
+		include_once './currency.php';
+		include_once './customer.php';
+		include_once './format.php';
+		include_once './gateways.php';
+		include_once './order.php';
+		include_once './subscriptions.php';
 	}
 }
