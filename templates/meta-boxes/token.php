24e7db757b3e9dc4835a8c38bb9e7be199cf883f
daniel21703 <daniel217032001@gmail.com>
2023-04-18 09:15:46 +0300

    add metaboxes 'token'  template

---
+++ templates/meta-boxes/token.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package Reepay\Checkout
+ *
+ * @var array $args arguments sent to template.
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+<ul class="order_action js-order_action">
+	<li class="reepay-admin-section-li">
+		<input type="text"
+			   value="<?php echo $args['token'] ?>"
+			   style="width:100%"
+			   data-reepay-token-value>
+	</li>
+	<li class="reepay-admin-section-li">
+		<button class="button" data-reepay-token-update>
+			<?php _e( 'Update ', 'reepay-subscriptions-for-woocommerce' ); ?>
+		</button>
+	</li>
+</ul>
