1e87b2d0bbba872d28ff59fbee7d1bfa6b5cb8b4
daniel21703 <daniel217032001@gmail.com>
2023-05-25 02:33:13 +0300

    add phpcs for tests

---
+++ bin/phpcs.tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+standard='--standard=./ruleset.xml'
+path='./tests'
+extra='--cache --colors -p -s' #remove colors if your terminal doesn't support them
+
+#extra+=' --report=diff -vvv' #uncomment for debug
+
+if [ "$1" == "-full" ]; then
+  php ./vendor/bin/phpcs $standard $path $extra
+elif [ "$1" == '-fix' ]; then
+  php ./vendor/bin/phpcbf $standard $path $extra
+else
+  php ./vendor/bin/phpcs --report=summary $standard $path $extra
+fi
