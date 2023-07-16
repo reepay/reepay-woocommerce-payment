62149b2332a379a99c2a8a1ab3aed8dc089a30ed
daniel21703 <daniel217032001@gmail.com>
2023-07-16 23:06:48 +0300

    add tests

--- tests/unit/orderFlow/OrderStatusesTest.php
+++ tests/unit/orderFlow/OrderStatusesTest.php
@@ -5,6 +5,7 @@
  * @package Reepay\Checkout
  */
 
+use Reepay\Checkout\Api;
 use Reepay\Checkout\OrderFlow\InstantSettle;
 
 use Reepay\Checkout\OrderFlow\OrderCapture;
@@ -345,7 +346,7 @@ class OrderStatusesTest extends WP_UnitTestCase {
 	 *
 	 * @dataProvider \Reepay\Checkout\Tests\Helpers\DataProvider::order_statuses()
 	 */
-	public function testget_authorized_order_status_with_sync( string $sync_status ) {
+	public function test_get_authorized_order_status_with_sync( string $sync_status ) {
 		$status = 'default_status';
 
 		$this->order_generator->set_prop( 'payment_method', reepay()->gateways()->checkout() );
@@ -370,26 +371,154 @@ class OrderStatusesTest extends WP_UnitTestCase {
 	public function test_set_authorized_status_already_authorized() {
 		$this->order_generator->set_meta('_reepay_state_authorized', 1);
 
-
+		$this->assertFalse( OrderStatuses::set_authorized_status( $this->order_generator->order() ) );
 	}
 
 	/**
-	 * Test @see OrderStatuses::set_authorized_status with _reepay_state_authorized meta
+	 * Test @see OrderStatuses::set_authorized_status to order with authorized status
 	 */
-	public function test_set_authorized_status_already_authorized2() {
+	public function test_set_authorized_status_to_order_with_authorized_status() {
+		$order_status = 'completed';
+
 		self::$options->set_options(
 			array(
 				'enable_sync'       => 'yes',
-				'status_authorized' => 'completed',
+				'status_authorized' => $order_status,
 			)
 		);
+
+		$this->order_generator->set_props( array(
+			'status' => $order_status,
+			'payment_method' => reepay()->gateways()->checkout()
+		) );
+
+		$this->assertFalse( OrderStatuses::set_authorized_status( $this->order_generator->order() ) );
+	}
+
+	/**
+	 * Test @see OrderStatuses::set_authorized_status
+	 */
+	public function test_set_authorized_status() {
+		$expected_status = 'completed';
+		$default_status = 'on-hold';
+
+		self::$options->set_options(
+			array(
+				'enable_sync'       => 'yes',
+				'status_authorized' => $expected_status,
+			)
+		);
+
+		$this->order_generator->set_props( array(
+			'status' => $default_status,
+			'payment_method' => reepay()->gateways()->checkout()
+		) );
+
+		$this->assertTrue( OrderStatuses::set_authorized_status( $this->order_generator->order() ) );
+		$this->assertSame( 1, $this->order_generator->get_meta( '_reepay_state_authorized' ) );
+		$this->assertSame( $expected_status, $this->order_generator->order()->get_status() );
+	}
+
+	/**
+	 * Test @see OrderStatuses::set_settled_status  with non reepay payment method
+	 */
+	public function test_set_settled_status_with_non_reepay_payment_method() {
+		$this->assertFalse( OrderStatuses::set_settled_status( $this->order_generator->order() ) );
+	}
+
+	/**
+	 * Test @see OrderStatuses::set_settled_status  with _reepay_state_settled meta
+	 */
+	public function test_set_settled_status_with_already_settled_order_meta() {
+		$this->order_generator->set_prop('payment_method', reepay()->gateways()->checkout());
+		$this->order_generator->set_meta('_reepay_state_settled', 1);
+
+		$this->assertFalse( OrderStatuses::set_settled_status( $this->order_generator->order() ) );
+	}
+
+	/**
+	 * Test @see OrderStatuses::set_settled_status  with api error
+	 */
+	public function test_set_settled_status_with_api_error() {
+		$this->order_generator->set_prop('payment_method', reepay()->gateways()->checkout());
+
+		$api_mock = $this->getMockBuilder( Api::class )->getMock();
+		$api_mock->method( 'get_invoice_data' )->willReturn(new WP_Error());
+		reepay()->di()->set( Api::class, $api_mock );
+
+
+		$this->assertFalse( OrderStatuses::set_settled_status( $this->order_generator->order() ) );
+	}
+
+	/**
+	 * Test @see OrderStatuses::set_settled_status  with data for authorized status from api
+	 */
+	public function test_set_settled_status_with_authorized_data() {
+		$expected_status = 'completed';
+		$default_status = 'on-hold';
+
+		$this->order_generator->set_props( array(
+			'status' => $default_status,
+			'payment_method' => reepay()->gateways()->checkout()
+		) );
+
+		self::$options->set_options(
+			array(
+				'enable_sync'       => 'yes',
+				'status_authorized' => $expected_status,
+			)
+		);
+
+		$api_mock = $this->getMockBuilder( Api::class )->getMock();
+		$api_mock->method( 'get_invoice_data' )->willReturn(
+			array(
+				'authorized_amount' => 100,
+				'settled_amount'    => 10
+			)
+		);
+		reepay()->di()->set( Api::class, $api_mock );
+
+		$this->assertTrue( OrderStatuses::set_settled_status( $this->order_generator->order() ) );
+		$this->assertSame( 1, $this->order_generator->get_meta( '_reepay_state_authorized' ) );
+		$this->assertSame( $expected_status, $this->order_generator->order()->get_status() );
 	}
 
 	/**
 	 * Test @see OrderStatuses::set_settled_status
 	 */
 	public function test_set_settled_status() {
+		$order_note = 'Test note';
+		$order_transaction_id = 'test_2077';
 
+		$expected_status = 'processing';
+		$default_status = 'on-hold';
+
+		$this->order_generator->set_props( array(
+			'status' => $default_status,
+			'payment_method' => reepay()->gateways()->checkout()
+		) );
+
+		self::$options->set_options(
+			array(
+				'enable_sync'       => 'yes',
+				'status_authorized' => $expected_status,
+			)
+		);
+
+		$api_mock = $this->getMockBuilder( Api::class )->getMock();
+		$api_mock->method( 'get_invoice_data' )->willReturn(
+			array(
+				'authorized_amount' => 100,
+				'settled_amount'    => 100
+			)
+		);
+		reepay()->di()->set( Api::class, $api_mock );
+
+		$this->assertTrue( OrderStatuses::set_settled_status( $this->order_generator->order(), $order_note, $order_transaction_id ), 'Status not settled' );
+		$this->assertSame( $expected_status, $this->order_generator->order()->get_status(), 'Wrong order status' );
+		$this->assertSame( $order_transaction_id, $this->order_generator->order()->get_transaction_id(), 'Wrong transaction id' );
+		$this->assertTrue( $this->order_generator->note_exists( $order_note ), 'Order note not added' );
+		$this->assertSame( 1, $this->order_generator->get_meta( '_reepay_state_settled' ), '_reepay_state_settled meta not settled' );
 	}
 
 	/**
@@ -406,8 +535,6 @@ class OrderStatusesTest extends WP_UnitTestCase {
 			$transaction_id,
 			true
 		);
-
-		$this->order_generator->reset_order();
 
 		$this->assertSame( $order_status, $this->order_generator->order()->get_status() );
 		$this->assertSame( $transaction_id, $this->order_generator->order()->get_transaction_id() );
