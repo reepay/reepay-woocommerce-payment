2e7eead60bb9bbf6a9252b99963db55741c26424
daniel21703 <daniel217032001@gmail.com>
2023-07-22 17:11:07 +0300

    update ReepayCustomer class

--- includes/Actions/ReepayCustomer.php
+++ includes/Actions/ReepayCustomer.php
@@ -20,7 +20,16 @@ class ReepayCustomer {
 	 * ReepayCustomer constructor.
 	 */
 	public function __construct() {
-		add_action( 'user_register', array( $this, 'set_reepay_handle' ), 1000, 1 );
+		add_action( 'user_register', array( $this, 'user_register' ), 1000, 1 );
+	}
+
+	/**
+	 * Action user_register
+	 *
+	 * @param int $user_id
+	 */
+	public function user_register( int $user_id ) {
+		self::set_reepay_handle( $user_id );
 	}
 
 	/**
@@ -28,25 +37,29 @@ class ReepayCustomer {
 	 *
 	 * @param int $user_id registered customer id.
 	 */
-	public function set_reepay_handle( int $user_id ) {
+	public static function set_reepay_handle( int $user_id ): string {
 		try {
 			$customer = new WC_Customer( $user_id );
 		} catch ( Exception $e ) {
-			return;
+			return '';
 		}
 
 		$email = $customer->get_email() ?: $customer->get_billing_email();
 
 		if ( empty( $email ) ) {
-			return;
+			return '';
 		}
 
 		$reepay_customers = reepay()->api( 'reepay_user_register' )->request( 'GET', "https://api.reepay.com/v1/list/customer?email=$email" );
 
 		if ( is_wp_error( $reepay_customers ) || empty( $reepay_customers['content'][0] ) ) {
-			return;
+			return '';
 		}
 
-		update_user_meta( $user_id, 'reepay_customer_id', $reepay_customers['content'][0]['handle'] );
+		$customer_handle = $reepay_customers['content'][0]['handle'];
+
+		update_user_meta( $user_id, 'reepay_customer_id', $customer_handle );
+
+		return $customer_handle;
 	}
 }
