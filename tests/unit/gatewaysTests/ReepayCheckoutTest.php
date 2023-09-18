8142dcde1711cfddf9a8d3a383e8c4b0417072bf
dimaspolohov@yandex.ru <Zenitkms123>
2023-09-18 11:53:23 +0300

    Gateway tests

--- tests/unit/gatewaysTests/ReepayCheckoutTest.php
+++ tests/unit/gatewaysTests/ReepayCheckoutTest.php
@@ -244,49 +244,5 @@ class ReepayCheckoutTest extends Reepay_UnitTestCase {
 		);
 	}
 
-	/**
-	 * Test function rp_get_webhook_url
-	 *
-	 * Test @param bool $is_test use test or live reepay api keys.
-	 * Test @param string $handle handle of account.
-	 *
-	 * @see ReepayGateway::get_webhook_url
-	 */
-	/*
-	public function test_rp_get_webhook_url() {
-		self::$gateway->id = 'checkout';
-		$this->getMockBuilder( SitePress::class )->getMock();
-
-		$time = $this->getFunctionMock( __NAMESPACE__, 'apply_filters' );
-
-		$url = self::$gateway::get_webhook_url();
-	}*/
-
-
-	/**
-	 * Test function rp_is_webhook_configured
-	 *
-	 * @testWith
-	 * ["http://example.com/wc-api/WC_Gateway_Reepay/"]
-	 * ["http://example.com/wc-api/WC_Gateway_Reepay_Checkout/"]
-	 * ["http://example.com/wc-api/not_configured/"]
-	 * @see ReepayGateway::is_webhook_configured
-	 */
-	public function test_rp_is_webhook_configured( string $webhook_url ) {
-		self::$gateway->id = 'checkout';
-
-		$this->api_mock->method( 'request' )->willReturn(
-			array(
-				'urls'         => array(
-					$webhook_url,
-				),
-				'alert_emails' => array(
-					'test@test.com',
-				),
-			)
-		);
-
-		var_dump( self::$gateway->is_webhook_configured() );
-	}
 
 }
