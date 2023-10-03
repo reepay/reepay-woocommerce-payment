fb55af58567e3ac4870a821e3ff780c2dd92938f
daniel21703 <daniel217032001@gmail.com>
2023-10-03 14:36:24 +0300

    add empty key API error

--- includes/Api.php
+++ includes/Api.php
@@ -238,6 +238,16 @@ class Api {
 			$key = reepay()->get_setting( 'private_key' );
 		}
 
+		if( empty( $key ) ) {
+			return new WP_Error(
+				401,
+				sprintf(
+					__( 'Billwerk+: API key not specified. Specify it in <a href="%s" target="_blank">gateway settings</a>', 'reepay-checkout-gateway' ),
+					admin_url( 'admin.php?page=wc-settings&tab=checkout&section=reepay_checkout' )
+				)
+			);
+		}
+
 		$args = array(
 			'headers' => array(
 				'Accept'        => 'application/json',
