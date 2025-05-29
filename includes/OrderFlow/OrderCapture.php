09e74f1a2c5510bd89b14c73ed37f1d2caec8b39
ZenTBelL <peerasak.sane@htmail.com>
2025-05-29 10:35:46 +0700

    Fix coding standard.

--- includes/OrderFlow/OrderCapture.php
+++ includes/OrderFlow/OrderCapture.php
@@ -588,9 +588,8 @@ class OrderCapture {
 			return false;
 		}
 
-
 		// for payment method with only settle.
-		if( is_array( $invoice_data ) && !array_key_exists('authorized_amount', $invoice_data) && 'settled' == $invoice_data['state'] && array_key_exists('amount', $invoice_data) && array_key_exists('settled_amount', $invoice_data) && $invoice_data['settled_amount'] === $invoice_data['amount'] ) {
+		if ( is_array( $invoice_data ) && ! array_key_exists( 'authorized_amount', $invoice_data ) && 'settled' === $invoice_data['state'] && array_key_exists( 'amount', $invoice_data ) && array_key_exists( 'settled_amount', $invoice_data ) && $invoice_data['settled_amount'] === $invoice_data['amount'] ) {
 			return false;
 		}
 
@@ -754,15 +753,14 @@ class OrderCapture {
 	 */
 	public function check_capture_allowed( WC_Order $order ): bool {
 		if ( ! rp_is_order_paid_via_reepay( $order ) ||
-			 class_exists( WC_Reepay_Renewals::class ) && WC_Reepay_Renewals::is_order_contain_subscription( $order ) ) {
+			class_exists( WC_Reepay_Renewals::class ) && WC_Reepay_Renewals::is_order_contain_subscription( $order ) ) {
 			return false;
 		}
 
 		$invoice_data = reepay()->api( $order )->get_invoice_data( $order );
 
-
 		// for payment method with only settle.
-		if( is_array( $invoice_data ) && !array_key_exists('authorized_amount', $invoice_data) && 'settled' == $invoice_data['state'] && array_key_exists('amount', $invoice_data) && array_key_exists('settled_amount', $invoice_data) && $invoice_data['settled_amount'] === $invoice_data['amount'] ) {
+		if ( is_array( $invoice_data ) && ! array_key_exists( 'authorized_amount', $invoice_data ) && 'settled' === $invoice_data['state'] && array_key_exists( 'amount', $invoice_data ) && array_key_exists( 'settled_amount', $invoice_data ) && $invoice_data['settled_amount'] === $invoice_data['amount'] ) {
 			return false;
 		}
 
